b15a45e8b12007c973acda61caa5713cd2a9a54b
hamitmizrak <hamitmizrak@gmail.com>
2025-08-10 12:03:54 +0300

    RegisterDto RegisterEntity ValidationMessages.properties

--- src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
+++ src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
@@ -1,5 +1,10 @@
 package com.hamitmizrak.business.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -12,18 +17,40 @@ public class RegisterDto implements Serializable {
     private Date systemCreatedDate;
 
     // Nickname
+    @NotEmpty(message = "{register.nickname.validation.constraints.NotNull.message}")
     private String registerNickname;
 
     // Name
+    @NotEmpty(message = "{register.name.validation.constraints.NotNull.message}")
     private String registerName;
 
     // Surname
+    @NotEmpty(message = "{register.surname.validation.constraints.NotNull.message}")
     private String registerSurname;
 
+
+    /*
+    ^[A-Za-z0-9._%+-]+ → E-posta kullanıcı adı kısmında harf, rakam, nokta, alt çizgi vb. izin verir.
+    @ → @ sembolü zorunlu.
+    [A-Za-z0-9.-]+ → Domain adı kısmı (ör. gmail, yahoo).
+    \\. → Nokta (.) karakteri.
+    [A-Za-z]{2,6}$ → Uzantı kısmı (ör. com, net, org), 2 ile 6 karakter uzunluğunda.
+    Sonundaki $ → Stringin bitişini temsil eder.
+     */
     // Email Address
+    @NotEmpty(message = "{register.email.validation.constraints.NotNull.message}")
+    @Email(message = "{register.email.validation.constraints.regex.message}")
+    //@AnnotationUniqueEmailAddress // have to unique email address
+    /*@Pattern(
+            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$",
+            message = "Lütfen geçerli bir e-posta adresi giriniz"
+    )*/
     private String registerEmail;
 
     // Password
+    @NotEmpty(message = "{register.password.validation.constraints.NotNull.message}")
+    @Size(min = 7,max=15,  message = "{register.password.validation.constraints.MinMax.NotNull.message}")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).*$", message = "{register.password.pattern.validation.constraints.NotNull.message}")
     private String registerPassword;
 
     // Roles
